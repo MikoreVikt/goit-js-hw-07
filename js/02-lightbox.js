3358e0898044c9e323f59c63201e00c25fd6e09f
MikoreVikt <104897659+MikoreVikt@users.noreply.github.com>
2022-10-08 22:41:09 +0300

    adds second gallery

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainerRef = document.querySelector(`.gallery`);
+const galleryMarkup = createGalleryMarkup(galleryItems);
+galleryContainerRef.innerHTML = galleryMarkup;
+
+let lightbox;
+
+galleryContainerRef.addEventListener(`click`, clickOnImgElGallery);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item" href="${original}">
+        <img loading="lazy" class="gallery__image lazyload" src="${preview}" alt="${description}" />
+      </a>`;
+    })
+    .join("");
+}
+
+function clickOnImgElGallery(event) {
+  event.preventDefault();
+
+  lightbox = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+  });
+}
